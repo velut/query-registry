e89aa57f8e30aa0d72d21f9f8da9922287c021e0
velut <12040076+velut@users.noreply.github.com>
2020-07-15 17:49:02 +0200

    docs: add package documentation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,64 @@
+/**
+ * This package exports a class, {@link Registry}, which provides methods
+ * to query the {@link https://www.npmjs.com | npm registry}
+ * or a custom npm-like registry.
+ *
+ * @example
+ * Create the default registry backed by npm:
+ *
+ * ```typescript
+ * import { Registry } from 'query-registry';
+ *
+ * const registry = new Registry();
+ *
+ * // Output: `https://registry.npmjs.org`
+ * console.log(registry.registry);
+ * ```
+ *
+ * @example
+ * Create a custom registry:
+ *
+ * ```typescript
+ * import { Registry } from 'query-registry';
+ *
+ * const registry = new Registry({
+ *     registry: 'https://registry.example.com',
+ *     mirrors: ['https://mirror.example.com'],
+ *     api: 'https://api.example.com',
+ *     suggestionsAPI: 'https://suggestions.example.com',
+ *     cache: new Map(),
+ * });
+ *
+ * // Output: `https://registry.example.com`
+ * console.log(registry.registry);
+ * ```
+ *
+ * @example
+ * Get the package manifest for `query-registry`'s latest version:
+ *
+ * ```typescript
+ * import { Registry } from 'query-registry';
+ *
+ * (async () => {
+ *     const registry = new Registry();
+ *     const manifest = await registry.getPackageManifest('query-registry');
+ *
+ *     // Output: `query-registry`
+ *     console.log(manifest.name);
+ * })();
+ * ```
+ *
+ * @example
+ * Enable {@link https://www.npmjs.com/package/debug | debug messages}
+ * by setting the `DEBUG` environment variable to `query-registry`:
+ *
+ * ```bash
+ * $ DEBUG="query-registry"
+ * ```
+ *
+ * @packageDocumentation
+ */
+
 export * from './bug-tracker';
 export * from './cache';
 export * from './dist-info';
