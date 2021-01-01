b03c3f0e806ea77446a31083d82d7ad2bc8730f3
velut <12040076+velut@users.noreply.github.com>
2021-01-01 18:16:04 +0100

    feat: add common public registries

---
+++ src/data/registries.ts
@@ -0,0 +1,20 @@
+/**
+ * npm registry
+ *
+ * @see {@link https://registry.npmjs.org}
+ */
+export const npmRegistry = 'https://registry.npmjs.org';
+
+/** npm registry mirror by Cloudflare
+ *
+ * @see {@link https://npmjs.cf}
+ * @see {@link https://registry.npmjs.cf}
+ */
+export const cloudflareRegistry = 'https://registry.npmjs.cf';
+
+/**
+ * npm registry mirror by Yarn
+ *
+ * @see {@link https://registry.yarnpkg.com}
+ */
+export const yarnRegistry = 'https://registry.yarnpkg.com';
