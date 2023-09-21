d387d477fdcdd09a05cafd3441faedec8a85ba28
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-09-21 02:41:41 +0200

    chore: format file

--- schemas/registry_metadata_test.ts
+++ schemas/registry_metadata_test.ts
@@ -1,8 +1,16 @@
 import { assertEquals, assertThrows } from "../dev_deps.ts";
-import cfRegistryMetadata from "../fixtures/registry_metadata/cf.json" assert { type: "json" };
-import npmRegistryMetadata from "../fixtures/registry_metadata/npm.json" assert { type: "json" };
-import skimdbRegistryMetadata from "../fixtures/registry_metadata/skimdb.json" assert { type: "json" };
-import yarnRegistryMetadata from "../fixtures/registry_metadata/yarn.json" assert { type: "json" };
+import cfRegistryMetadata from "../fixtures/registry_metadata/cf.json" assert {
+  type: "json",
+};
+import npmRegistryMetadata from "../fixtures/registry_metadata/npm.json" assert {
+  type: "json",
+};
+import skimdbRegistryMetadata from "../fixtures/registry_metadata/skimdb.json" assert {
+  type: "json",
+};
+import yarnRegistryMetadata from "../fixtures/registry_metadata/yarn.json" assert {
+  type: "json",
+};
 import { registryMetadataSchema } from "./registry_metadata.ts";
 
 Deno.test("registryMetadataSchema with correct data", () => {
