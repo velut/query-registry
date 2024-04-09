9d19a0dbfe7cc52efd9ba68d95c4eea416e593bf
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-04-09 18:05:33 +0200

    feat: add DistTags

---
+++ src/dist-tags.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+/**
+Zod schema for the mapping of distribution tags to semver version numbers.
+*/
+export const distTagsSchema = z
+	.object({
+		latest: z.string(),
+	})
+	.catchall(z.string());
+
+/**
+Mapping of distribution tags to semver version numbers (e.g., `{ "latest": "1.0.0" }`).
+*/
+export type DistTags = z.infer<typeof distTagsSchema>;
