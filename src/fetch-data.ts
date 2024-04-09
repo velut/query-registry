7ae52410dc9d069a04c9bd727f1bdc5ecfceee01
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-04-09 18:03:16 +0200

    feat: add headers param to fetchData

--- src/fetch-data.ts
+++ src/fetch-data.ts
@@ -1,10 +1,11 @@
 import { z } from "zod";
 
 export const fetchData = async <T extends z.ZodTypeAny>(
-	url: string,
 	schema: T,
+	url: string,
+	headers?: Record<string, string>,
 ): Promise<z.TypeOf<T>> => {
-	const response = await fetch(url);
+	const response = await fetch(url, { headers });
 	const json = await response.json();
 	return schema.parse(json) as z.infer<T>;
 };
