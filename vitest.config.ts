19cfe6821545ba0e3fc76502510da3e44242189c
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-04-13 01:15:01 +0200

    test: do not test old code

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "vitest/config";
+import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
 	test: {
 		coverage: {
 			include: ["src/**"],
 		},
+		exclude: [...configDefaults.exclude, "old"],
 	},
 });
