221548641dd59bc0f82368f292645b16101c4b43
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-04-13 20:47:30 +0200

    feat: add getDailyRegistryDownloads

---
+++ src/get-daily-registry-downloads.ts
@@ -0,0 +1,46 @@
+import urlJoin from "url-join";
+import { z } from "zod";
+import { DownloadPeriod } from "./download-period";
+import { fetchData } from "./fetch-data";
+import { npmRegistryDownloadsApiUrl } from "./npm-registry";
+
+export const DailyRegistryDownloads = z.object({
+	/** Date of the first day (inclusive) in the format `YYYY-MM-DD`. */
+	start: z.string(),
+
+	/** Date of the last day (inclusive) in the format `YYYY-MM-DD`. */
+	end: z.string(),
+
+	/** Download counts for each day. */
+	downloads: z.array(
+		z.object({
+			/** Total number of downloads for the day. */
+			downloads: z.number(),
+
+			/** Date of the day in the format `YYYY-MM-DD`. */
+			day: z.string(),
+		}),
+	),
+});
+
+/**
+`DailyRegistryDownloads` describes the total number of downloads for each day
+for all packages in the registry in a given time period.
+@see {@link https://github.com/npm/registry/blob/master/docs/download-counts.md#ranges}
+*/
+export type DailyRegistryDownloads = z.infer<typeof DailyRegistryDownloads>;
+
+/**
+`getDailyRegistryDownloads` returns the total number of downloads for each day
+for all packages in the registry in a given time period.
+
+@param period - {@link DownloadPeriod | time period} in which downloads happened
+@param registry - URL of the registry downloads API (default: npm registry downloads API)
+
+@see {@link DailyRegistryDownloads}
+*/
+export const getDailyRegistryDownloads = async (
+	period: DownloadPeriod,
+	registry = npmRegistryDownloadsApiUrl,
+): Promise<DailyRegistryDownloads> =>
+	fetchData(DailyRegistryDownloads, urlJoin(registry, `/downloads/range/${period}`));
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ TODO:
 export { PackageJson } from "zod-package-json";
 export { DownloadPeriod } from "./download-period";
 export { AbbreviatedPackument, getAbbreviatedPackument } from "./get-abbreviated-packument";
+export { DailyRegistryDownloads, getDailyRegistryDownloads } from "./get-daily-registry-downloads";
 export { PackageDownloads, getPackageDownloads } from "./get-package-downloads";
 export { PackageManifest, getPackageManifest } from "./get-package-manifest";
 export { RegistryDownloads, getRegistryDownloads } from "./get-registry-downloads";
