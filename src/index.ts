96baaeef8221bbcad39b49985266d02b7e1a35d9
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-04-14 18:47:42 +0200

    feat: update exports from index file

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,18 @@ TODO:
 export { PackageJson } from "zod-package-json";
 export { DownloadPeriod } from "./download-period";
 export { AbbreviatedPackument, getAbbreviatedPackument } from "./get-abbreviated-packument";
+export {
+	BulkDailyPackageDownloads,
+	getBulkDailyPackageDownloads,
+} from "./get-bulk-daily-package-downloads";
+export { BulkPackageDownloads, getBulkPackageDownloads } from "./get-bulk-package-downloads";
 export { DailyRegistryDownloads, getDailyRegistryDownloads } from "./get-daily-registry-downloads";
 export { PackageDownloads, getPackageDownloads } from "./get-package-downloads";
 export { PackageManifest, getPackageManifest } from "./get-package-manifest";
+export {
+	PackageVersionsDownloads,
+	getPackageVersionsDownloads,
+} from "./get-package-versions-downloads";
 export { RegistryDownloads, getRegistryDownloads } from "./get-registry-downloads";
 export { RegistryMetadata, getRegistryMetadata } from "./get-registry-metadata";
 export { RegistrySigningKeys, getRegistrySigningKeys } from "./get-registry-signing-keys";
