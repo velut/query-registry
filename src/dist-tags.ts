1d234a48fb56909a696a4e7a045e06ed2f458f0f
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-04-16 19:27:15 +0200

    feat: add common optional dist tags

--- src/dist-tags.ts
+++ src/dist-tags.ts
@@ -6,6 +6,16 @@ import { z } from "zod";
 */
 export const DistTags = z
 	.object({
+		/** Latest semver version number. */
 		latest: z.string(),
+
+		// The following tags have no special meaning for the npm registry
+		// but they are commonly used by packages.
+		next: z.string().optional(),
+		alpha: z.string().optional(),
+		beta: z.string().optional(),
+		rc: z.string().optional(),
+		canary: z.string().optional(),
+		dev: z.string().optional(),
 	})
 	.catchall(z.string());
