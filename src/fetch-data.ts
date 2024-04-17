e7e749abed296280284af219270f34c2feba875c
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-04-18 00:35:10 +0200

    feat: add caching

--- src/fetch-data.ts
+++ src/fetch-data.ts
@@ -1,11 +1,26 @@
+import QuickLRU from "quick-lru";
 import { z } from "zod";
+
+const cache = new QuickLRU({
+	// 100 items.
+	maxSize: 100,
+
+	// 5 minutes.
+	maxAge: 5 * 60 * 1000,
+});
 
 export const fetchData = async <T extends z.ZodTypeAny>(
 	schema: T,
 	url: string,
 	headers?: Record<string, string>,
 ): Promise<z.TypeOf<T>> => {
+	const cacheKey = JSON.stringify({ url, headers });
+	const cachedJson = cache.get(cacheKey);
+	if (cachedJson) {
+		return schema.parse(cachedJson) as z.infer<T>;
+	}
 	const response = await fetch(url, { headers });
-	const json = await response.json();
+	const json = (await response.json()) as unknown;
+	cache.set(cacheKey, json);
 	return schema.parse(json) as z.infer<T>;
 };
