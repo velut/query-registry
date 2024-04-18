0ae5728414bba51cae2752fe3f5d2fbc98ff4235
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-04-19 01:50:37 +0200

    chore: add JsonStrip util

---
+++ utils/json-strip.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+const Literal = z.union([
+	// Truncate strings and numbers to save space in the test DB.
+	z.string().transform(() => ""),
+	z.number().transform(() => 0),
+	z.boolean(),
+	z.null(),
+]);
+
+type Literal = z.infer<typeof Literal>;
+
+export type JsonStrip = Literal | { [key: string]: JsonStrip } | JsonStrip[];
+
+export const JsonStrip: z.ZodType<JsonStrip> = z.lazy(() =>
+	z.union([Literal, z.array(JsonStrip), z.record(JsonStrip)]),
+);
