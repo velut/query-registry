43dcb525313044826c3792d98d300fe6118b84dd
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-04-19 02:25:11 +0200

    test: increase test timeout

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
 	test: {
+		testTimeout: 10000,
 		coverage: { include: ["src/**"] },
 		exclude: [...configDefaults.exclude, "old"],
 	},
