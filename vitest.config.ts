e52f865efbacd5f46df2aef9867babde0d23ae0d
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-04-22 19:06:49 +0200

    chore: remove references to deleted dirs

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,8 @@
-import { configDefaults, defineConfig } from "vitest/config";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	test: {
 		testTimeout: 10000,
 		coverage: { include: ["src/**"] },
-		exclude: [...configDefaults.exclude, "old"],
 	},
 });
