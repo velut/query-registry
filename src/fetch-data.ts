9d4a10fdff0b0a92be34bb1823a61cb2a69df9e4
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-08-19 17:59:21 +0200

    feat: update fetchData zod types

--- src/fetch-data.ts
+++ src/fetch-data.ts
@@ -1,18 +1,16 @@
-import type { z } from "zod";
+import type z from "zod";
 import { cache } from "./cache";
 
-export const fetchData = async <T extends z.ZodTypeAny>(
+export const fetchData = async <T extends z.ZodType>(
 	schema: T,
 	url: string,
 	headers?: Record<string, string>,
-): Promise<z.TypeOf<T>> => {
+): Promise<z.infer<T>> => {
 	const cacheKey = JSON.stringify({ url, headers });
 	const cachedJson = cache.get(cacheKey);
-	if (cachedJson) {
-		return schema.parse(cachedJson) as z.infer<T>;
-	}
+	if (cachedJson) return schema.parse(cachedJson);
 	const response = await fetch(url, { headers });
 	const json = (await response.json()) as unknown;
 	cache.set(cacheKey, json);
-	return schema.parse(json) as z.infer<T>;
+	return schema.parse(json);
 };
