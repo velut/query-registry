e1c705c51693c1852aa3eef6294cd0f720b1cc91
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-08-21 20:28:57 +0200

    refactor: use inline if body

--- src/assert-valid-package-name.ts
+++ src/assert-valid-package-name.ts
@@ -3,7 +3,5 @@ import validatePackageName from "validate-npm-package-name";
 export const assertValidPackageName = (name: string) => {
 	const { validForOldPackages, validForNewPackages, warnings, errors } = validatePackageName(name);
 	const isValid = validForOldPackages || validForNewPackages;
-	if (!isValid) {
-		throw new Error("invalid package name", { cause: { name, warnings, errors } });
-	}
+	if (!isValid) throw new Error("invalid package name", { cause: { name, warnings, errors } });
 };
